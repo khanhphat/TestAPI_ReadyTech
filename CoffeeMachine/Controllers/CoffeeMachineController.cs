2e43df749cd5a6b54eace11064f6f05279f6fe73
DESKTOP-LTUG14J\user <nguyenhuukhanhphat@gmail.com>
2022-02-22 07:26:48 +0700

    Implement HTTP api

---
+++ CoffeeMachine/Controllers/CoffeeMachineController.cs
@@ -0,0 +1,63 @@
+﻿using CoffeeMachine.Common;
+using CoffeeMachine.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CoffeeMachine.Controllers
+{
+    [ApiController]
+    public class CoffeeMachineController : ControllerBase
+    {
+        // GET: api/<CoffeeMachineController>
+        [Route("brew-coffee")]
+        [HttpGet]
+        public ActionResult<CoffeeCup> Get()
+        {
+            AccessCount.Instance.Count();
+
+            if (AccessCount.Instance.AccessNumber % 5 == 0)
+            {
+                return StatusCode(503);
+            }
+
+            if (DateTime.Now.Day == 1 && DateTime.Now.Month == 4)
+            {
+                return StatusCode(418);
+            }
+
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri("https://api.openweathermap.org/data/2.5/");
+
+            var task = Task.Run(async () => {
+                var respondata = await client.GetAsync("weather?q=London&units=metric&appid=" + Contants.ApiKey).ConfigureAwait(false);
+                var data = await respondata.Content.ReadAsStringAsync();
+                return data;
+            });
+
+            task.Wait();
+            string result = task.Result;
+
+            CoffeeCup coffeeCup = new CoffeeCup();
+            if (!string.IsNullOrEmpty(result))
+            {
+                WeatherBase weatherBase = JsonConvert.DeserializeObject<WeatherBase>(result);
+                Main main = new Main();
+                main = weatherBase.Main;
+                if (main.Temp > 30)
+                {
+                    coffeeCup.Message = "Your refreshing iced coffee is ready";
+                }
+            }
+            return coffeeCup;
+        }
+
+    }
+}
---
+++ CoffeeMachine/Startup.cs
@@ -0,0 +1,74 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace CoffeeMachine
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseStatusCodePages(appError => {
+                appError.Run(async context => {
+                    var response = context.Response;
+                    var code = response.StatusCode;
+                    var content = @$"<html>
+                        <head>
+                            <meta charset='UTF-8'/>
+                            <title>Error {code}</title>
+                        </head>
+                        <body>An error occurred: {code} - {(HttpStatusCode)code}</body>
+                    </html>";
+                    await response.WriteAsync(content);
+                });
+            }); //return status 400-599
+
+            app.UseRouting(); //endpointRoutingMiddleware
+
+            app.UseAuthorization(); //xac dinh danh tinh
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+               
+            });
+
+            //app.Run(async context => {
+            //    context.Response.StatusCode = StatusCodes.Status404NotFound;
+            //    await context.Response.WriteAsync("Page not found");
+            //});
+        }
+    }
+}
